ce17f3fb445ba29ea2b16c2e7b6edb71bd541853
ssachan <shikhar.sachan@gmail.com>
2012-11-05 12:36:38 +0530

    some minor changes

--- client/config.js
+++ client/config.js
@@ -1,114 +1,21 @@
-var SEPARATOR = '|:';
-
-window.Storage = {
-	    LOCALSTORAGE : 0,
-	    WEBSQL : 1,
-	    FILESYSTEM : 2
-};
-
-var config_mobile_local = {
-	syncStoragePath : "",
-	templatePath : "",
-	serverUrl : 'http://localhost/nero/api/',
-	edition : '0',
-	editionName : 'mobile',
-	assetPath : '',
-    tmplatesFolder : "tpl-mobile",
+var Config = {
+    serverUrl : 'http://localhost/nero/newcode/api/',
+    edition : '1',
+    editionName : 'web',
+    assetPath : '',
+    tmplatesFolder : "templates",
     phonegap : false,
-    viewsArray : [	'LandingView', 
-    				'QuizQuestionView',
-    				'FlashCardListView',
-    				'FlashCardListItemView',
-    				'FlashCardView',
-    				'MenuView',
-    				'PracticeTopicsView',
-    				'PracticeQuestionView',
-    				'QuizTopicsView',
-    				'QuizResultsView',
-    				'ProfileView',
-    			]
+    viewsArray : [  
+                    'HeaderView',
+                    'LandingView',
+                    'QuizView',
+                    'QuizQuestionView',
+                    'QuizResultsView',
+                    'DashboardView',
+                    'QuizLibraryView',
+                    'QuizItemView',
+                    'FacDirectoryView',
+                    'FacItemView',
+                    'DashboardView'
+                ]
 };
-
-var config_web_local = {
-	syncStoragePath : "",
-	templatePath : "",
-	serverUrl : 'http://localhost/nero/api/',
-	edition : '1',
-	editionName : 'web',
-	assetPath : '',
-    tmplatesFolder : "tpl-web",
-    phonegap : false,
-    viewsArray : [	
-    				'HeaderView',
-                  	'LandingView',
-                  	'MenuView',
-                  	'QuizView',
-    				'QuizQuestionView',
-    				'FlashCardListView',
-    				'FlashCardListItemView',
-    				'FlashCardView',
-    				'MenuView',
-    				'MainView',
-    				'PracticeTopicsView',
-    				'PracticeView',
-    				'PracticeQuestionView',
-    				'QuizTopicsView',
-    				'QuizResultsView',
-    				'ProfileView',
-    			]
-};
-
-
-var config_mobile_server = {
-		syncStoragePath : "",
-		templatePath : "",
-		serverUrl : 'http://www.test-rex.com/prod/api/',
-		edition : '0',
-		editionName : 'mobile',
-		assetPath : '',
-	    tmplatesFolder : "tpl-mobile",
-	    phonegap : false,
-	    viewsArray : [	'LandingView', 
-	    				'QuizQuestionView',
-	    				'FlashCardListView',
-	    				'FlashCardListItemView',
-	    				'FlashCardView',
-	    				'MenuView',
-	    				'PracticeTopicsView',
-	    				'PracticeQuestionView',
-	    				'QuizTopicsView',
-	    				'QuizResultsView',
-	    				'ProfileView',
-	    			]
-	};
-
-var config_web_server = {
-		syncStoragePath : "",
-		templatePath : "",
-		serverUrl : 'http://www.test-rex.com/prod/api/',
-		edition : '1',
-		editionName : 'web',
-		assetPath : '',
-	    tmplatesFolder : "tpl-web",
-	    phonegap : false,
-	    viewsArray : [	
-	    				'HeaderView',
-	                  	'LandingView',
-	                  	'MenuView',
-	                  	'QuizView',
-	    				'QuizQuestionView',
-	    				'FlashCardListView',
-	    				'FlashCardListItemView',
-	    				'FlashCardView',
-	    				'MenuView',
-	    				'MainView',
-	    				'PracticeTopicsView',
-	    				'PracticeView',
-	    				'PracticeQuestionView',
-	    				'QuizTopicsView',
-	    				'QuizResultsView',
-	    				'ProfileView',
-	    			]
-	};
-
-var Config = config_web_local; 
